93d54569d8a83b90d740db29b357f60465641f65
Timmy <timilehinoluwaseyi07@gmail.com>
2025-05-17 13:05:33 +0100

    Refactor API key handling and update LLM model version; modify route definitions for consistency,
    Reduced the amount of requirements in the requirements.txt file

--- ai.py
+++ ai.py
@@ -13,7 +13,9 @@ load_dotenv()
 
 app = Flask(__name__)
 
-api_key = os.getenv('API_KEY')
+api_key = os.getenv("API_KEY")
+
+
 # Initialize the LLM and required components
 def initialize_llm():
     documents = SimpleDirectoryReader("./data").load_data()
@@ -22,12 +24,14 @@ def initialize_llm():
 
     Settings.llm = Gemini(
         api_key=api_key,
-        model="models/gemini-pro",
+        model="models/gemini-2.0-flash",
         temperature=0.9,
     )
     Settings.node_parser = SentenceSplitter(chunk_size=1000, chunk_overlap=20)
     Settings.embed_model = GeminiEmbedding(
-        model_name="models/embedding-001", api_key=api_key, title="this is a new document"
+        model_name="models/embedding-001",
+        api_key=api_key,
+        title="this is a new document",
     )
 
     vector_store = ChromaVectorStore(chroma_collection)
@@ -37,7 +41,7 @@ def initialize_llm():
 
     template = (
         "We have provided context information below which will serve as a user's transaction history."
-        "With \"Message\" being the category that the user has spent his or her money on. Transfer Sent means that the money leaves the users account and deposit successful means money that comes into the users account and Savings Lock Created means the money the user has saved. The currency being used is Naira. Any question that is not finance related and is not based on the users transaction history should be replied with: \"I cannot provide you with information on that\"\n"
+        'With "Message" being the category that the user has spent his or her money on. Transfer Sent means that the money leaves the users account and deposit successful means money that comes into the users account and Savings Lock Created means the money the user has saved. The currency being used is Naira. Any question that is not finance related and is not based on the users transaction history should be replied with: "I cannot provide you with information on that"\n'
         "---------------------\n"
         "{context_str}"
         "\n---------------------\n"
@@ -49,12 +53,14 @@ def initialize_llm():
     query_engine = index.as_query_engine(text_qa_template=qa_template)
     return query_engine
 
+
 query_engine = initialize_llm()
 
-@app.route('/query', methods=['POST'])
+
+@app.route("/query", methods=["POST"])
 def query():
     data = request.json
-    query = data.get('query', '')
+    query = data.get("query", "")
 
     if query:
         response = query_engine.query(query)
@@ -63,54 +69,48 @@ def query():
         return jsonify({"error": "No query provided"}), 400
 
 
-@app.route('/getTransactionLog',methods=['POST'])
+@app.route("/getTransactionLog", methods=["POST"])
 def get_transaction_log():
     try:
         data = request.json
-        query = data.get('query','')
+        query = data.get("query", "")
 
         print("recived transactions")
 
-        with open('./data/recieved_data.txt', 'a', encoding='utf-8') as f:
-            f.write('\n' + query + '\n')
+        with open("./data/recieved_data.txt", "a", encoding="utf-8") as f:
+            f.write("\n" + query + "\n")
 
-            return jsonify(
-                {
-                    'success': True,
-                    'message': "Log Saved Successfully"
-                }
-            ), 200
+            return jsonify({"success": True, "message": "Log Saved Successfully"}), 200
     except Exception as e:
         print("Error")
-        return jsonify(
-            {
-                'error':str(e)
-            }
-        ),500
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/sendAnalysis', methods = ['POST'])
+
+@app.route("/sendAnalysis", methods=["POST"])
 def sendAnalysis():
 
-    with open('./data/recieved_data.txt', 'r', encoding = 'utf-8') as f:
+    with open("./data/recieved_data.txt", "r", encoding="utf-8") as f:
         content = f.read()
 
     try:
-        analysis = query_engine.query('Analyze the transaction history provided below and give a comprehensive analysis on my spending and also give some advice how to spend the money better. The context to analyze:' + content)
+        analysis = query_engine.query(
+            "Analyze the transaction history provided below and give a comprehensive analysis on my spending and also give some advice how to spend the money better. The context to analyze:"
+            + content
+        )
 
-        return jsonify(
-            {
-                'success': True,
-                'analysis': str(analysis),
-
-            }
-        ), 200
+        return (
+            jsonify(
+                {
+                    "success": True,
+                    "analysis": str(analysis),
+                }
+            ),
+            200,
+        )
     except Exception as e:
         print("error in sending analysis")
-        return jsonify({
-            "success": False,
-            "error":str(e)
-        }), 500
+        return jsonify({"success": False, "error": str(e)}), 500
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
